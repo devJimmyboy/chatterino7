86363a84ac3eb97b65b5e76b0ebd4b7507aeb841
nerix <nerixdev@outlook.de>
2024-01-18 20:50:57 +0100

    fix: assert for GUI thread in getters instead of `get(I)App` (#5102)

--- src/Application.cpp
+++ src/Application.cpp
@@ -307,26 +307,35 @@ int Application::run(QApplication &qtApp)
 
 IEmotes *Application::getEmotes()
 {
+    assertInGuiThread();
+
     return this->emotes;
 }
 
 IUserDataController *Application::getUserData()
 {
+    assertInGuiThread();
+
     return this->userData;
 }
 
 ISoundController *Application::getSound()
 {
+    assertInGuiThread();
+
     return this->sound;
 }
 
 ITwitchLiveController *Application::getTwitchLiveController()
 {
+    assertInGuiThread();
+
     return this->twitchLiveController;
 }
 
 TwitchBadges *Application::getTwitchBadges()
 {
+    assertInGuiThread();
     assert(this->twitchBadges);
 
     return this->twitchBadges.get();
@@ -334,16 +343,22 @@ TwitchBadges *Application::getTwitchBadges()
 
 ITwitchIrcServer *Application::getTwitch()
 {
+    assertInGuiThread();
+
     return this->twitch;
 }
 
 PubSub *Application::getTwitchPubSub()
 {
+    assertInGuiThread();
+
     return this->twitchPubSub.get();
 }
 
 Logging *Application::getChatLogger()
 {
+    assertInGuiThread();
+
     return this->logging.get();
 }
 
@@ -925,16 +940,12 @@ Application *getApp()
 {
     assert(Application::instance != nullptr);
 
-    assertInGuiThread();
-
     return Application::instance;
 }
 
 IApplication *getIApp()
 {
     assert(IApplication::instance != nullptr);
-
-    assertInGuiThread();
 
     return IApplication::instance;
 }
